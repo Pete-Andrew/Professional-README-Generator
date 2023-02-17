6f090592703d679d0f09a49a681c9304f27a1067
Pete Andrew <112977421+Pete-Andrew@users.noreply.github.com>
2023-02-17 11:54:51 +0000

    added email and github sections

--- README.txt
+++ README.txt
@@ -1,18 +1,22 @@
-##Project Title: 
-    undefined
-##Description: 
-    r
-##Table of Contents: 
-    r
-##Installation: 
-    r
-##Usage: 
-    r
-##Liscence: 
-    MIT
-##Contributing: 
-    r
-##Tests: 
-    r
-##Questions: 
-    r
+# Project Title: 
+undefined
+## Description: 
+r
+## Table of Contents: 
+r
+## Installation: 
+r
+## Usage: 
+r
+## Liscence: 
+MIT
+## Contributing: 
+r
+## Tests: 
+r
+## Questions: 
+
+If you would like to learn more about this project or my programming please see the following: 
+GitHub: r, 
+If you would like to contact me please use the following email address:
+Email: r
--- index.js
+++ index.js
@@ -46,8 +46,13 @@ inquirer
         },
         {
             type: 'input',
-            message: 'Questions',
-            name: 'questions'
+            message: 'Please enter your GitHub user name and link to GitHub profile',
+            name: 'questionsGithub'
+        },
+        {
+            type: 'input',
+            message: 'Please enter your email address',
+            name: 'questionsEmail'
         },
     ])
     .then((response) => {
@@ -56,30 +61,46 @@ inquirer
 
 const usersInfo = 
 
-`##Project Title: 
+`# Project Title: 
 ${response.projectTitle}
-##Description: 
+## Description: 
 ${response.description}
-##Table of Contents: 
+## Table of Contents: 
 ${response.ToC}
-##Installation: 
+## Installation: 
 ${response.installation}
-##Usage: 
+## Usage: 
 ${response.usage}
-##Liscence: 
+## Liscence: 
 ${response.liscence}
-##Contributing: 
+## Contributing: 
 ${response.contributing}
-##Tests: 
+## Tests: 
 ${response.tests}
-##Questions: 
-${response.questions}
+## Questions: 
+
+If you would like to learn more about this project or my programming please see the following: 
+GitHub: ${response.questionsGithub} 
+If you would like to contact me please use the following email address:
+Email: ${response.questionsEmail}
 `;
 
         fs.writeFile('README.txt', usersInfo, (err) =>
-            err ? console.error(err) : console.log('User\'s info logged!')
+            err ? console.error(err) : console.log('Thank you for entering data!')
         )
     })
 
+
+// When a user enters the project title, it's displayed as the title of the README.
+
+// When a user enters a description, installation instructions, usage information, contribution guidelines, and test instructions, this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests.
+
+// When a user chooses a license for their application from a list of options, a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under.
+
+// When a user enters their GitHub username, it's added to the section of the README entitled Questions, with a link to their GitHub profile.
+
+// When a user enters their email address, it's added to the section of the README entitled Questions, with instructions on how to reach them with additional questions.
+
+// When a user clicks on the links in the Table of Contents, they are taken to the corresponding section of the README.
 
 //homework overview: https://courses.bootcampspot.com/courses/3018/assignments/44672?module_item_id=831886
